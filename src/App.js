990805b90f5057cfc297fb17e1ff564aa19a33b6
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-06-15 07:28:06 +0900

    feature: add styles directory and move css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import ResultList from "./components/resultList";
-import "./App.css";
+import "./styles/App.css";
 
 const App = () => {
   const searchResults = [];
