aac78ebde434621ca8ea4e991d133ebb5e85efe6
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-07-07 04:41:46 +0900

    feature: improve layouts.

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,11 @@ const header = () => {
     <header>
       <h1 className="header__ttl">ARUKANA</h1>
       <p className="header__desc">
-        周辺施設までの距離を一括検索できるサービスです
+        周辺施設までの距離を一括検索
+        <br className="header__br" />
+        できるサービスです。
         <br />
-        引っ越しや旅行にお役立てください
+        引っ越しや旅行にお役立てください。
       </p>
     </header>
   );
