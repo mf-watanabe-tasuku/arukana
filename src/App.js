398d17f9c18533e450820664a92a71c42f5312cf
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-06-05 20:23:33 +0900

    refactor: remove LoadingState import

--- src/App.js
+++ src/App.js
@@ -4,26 +4,23 @@ import TopPage from './pages/TopPage';
 import NotFound from './pages/NotFound';
 import SearchState from './context/search/SearchState';
 import ResultState from './context/result/ResultState';
-import LoadingState from './context/loading/LoadingState';
 import './styles/App.css';
 
 const App = () => {
   return (
     <SearchState>
       <ResultState>
-        <LoadingState>
-          <Router>
-            <Header />
-            <main>
-              <div className='wrapper'>
-                <Switch>
-                  <Route exact path='/' component={TopPage} />
-                  <Route component={NotFound} />
-                </Switch>
-              </div>
-            </main>
-          </Router>
-        </LoadingState>
+        <Router>
+          <Header />
+          <main>
+            <div className='wrapper'>
+              <Switch>
+                <Route exact path='/' component={TopPage} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </main>
+        </Router>
       </ResultState>
     </SearchState>
   );
