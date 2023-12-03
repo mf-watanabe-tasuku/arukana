599732ac0a0e9191ca8be4510fb689e5b647405d
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-12-03 23:14:02 +0900

    Move layout CSS to styled-components

--- src/App.js
+++ src/App.js
@@ -5,21 +5,38 @@ import NotFound from './pages/NotFound';
 import SearchState from './context/search/SearchState';
 import ResultState from './context/result/ResultState';
 import './styles/App.css';
+import { styled } from 'styled-components';
+
+const StyleWrapper = styled.div`
+  padding: 50px 30px 100px;
+  background-color: #f5f6f9;
+
+  @media (max-width: 767px) {
+    padding: 30px 15px 50px;
+  }
+`;
+
+const StyledContainer = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`;
 
 const App = () => {
   return (
     <SearchState>
       <ResultState>
         <Router>
-          <Header />
-          <main>
-            <div className='wrapper'>
-              <Switch>
-                <Route exact path='/' component={TopPage} />
-                <Route component={NotFound} />
-              </Switch>
-            </div>
-          </main>
+          <StyleWrapper>
+            <StyledContainer>
+              <Header />
+              <main>
+                <Switch>
+                  <Route exact path='/' component={TopPage} />
+                  <Route component={NotFound} />
+                </Switch>
+              </main>
+            </StyledContainer>
+          </StyleWrapper>
         </Router>
       </ResultState>
     </SearchState>
