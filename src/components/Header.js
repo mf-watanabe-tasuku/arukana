899b5506c6146ce8101e8818c4b243154ee4cfb3
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-12-03 22:27:08 +0900

    move header CSS to styled-components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,33 @@
-import '../styles/Header.css';
+import { styled } from 'styled-components';
+
+const StyledHeader = styled.header`
+  text-align: center;
+`;
+
+const StyledHeaderTitle = styled.h1`
+  font-size: 50px;
+  font-family: 'Kanit', sans-serif;
+  line-height: 1;
+  color: #003972;
+  margin-bottom: 20px;
+`;
+
+const StyledHeaderDesc = styled.p`
+  margin-bottom: 50px;
+`;
 
 const Header = () => {
   return (
-    <header>
-      <h1 className='header__ttl'>ARUKANA</h1>
-      <p className='header__desc'>
+    <StyledHeader>
+      <StyledHeaderTitle>ARUKANA</StyledHeaderTitle>
+      <StyledHeaderDesc>
         周辺施設までの距離を一括検索
-        <br className='header__br' />
+        <br className='show-sp'/>
         できるサービスです。
         <br />
         引っ越しや旅行にお役立てください。
-      </p>
-    </header>
+      </StyledHeaderDesc>
+    </StyledHeader>
   );
 };
 
